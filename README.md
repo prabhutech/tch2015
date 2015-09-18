e20a8949add0c652241d7fa86810d2d4af3a502d
Prabhakar Doraisamy <prabhutech@yahoo.com>
2015-09-18 22:49:05 +0000

    first shot at rest get

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tch2015
+
+#Weather underground api
+http://api.wunderground.com/api/b44b1327c40539f8/forecast10day/q/CA/San_Francisco.json
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,6 +13,13 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.get('/getmestubdata', function (req, res) {
+   fs.readFile( __dirname + "/public/" + "data.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
