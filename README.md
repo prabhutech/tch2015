3a0a58820c39409a3f050fecd31b93362640901d
Prabhakar Doraisamy <prabhutech@yahoo.com>
2015-09-20 05:32:07 +0000

    work in progress

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 #Weather underground api
 http://api.wunderground.com/api/b44b1327c40539f8/forecast10day/q/CA/San_Francisco.json
+
+IF your are like me..
+
+Think about how much time is wasted in mundane permutations of your wardrobe selection ... and you are still not o n top of it.
+Introducing TenOnTen - the perfect solution for your personalized wardrobe. 
+If every ticking second is money....  TenOnTen is your money saver.
+
+TenOnTen recommends based on
+- your "individual choices" (show preferences tab)
+- your "day's schedule" 
+- "what's in your wardrobe" ....   
+- And extraneous factors like "weather and venue".
+- 
+Moreover TenTen even helps you grow your wardrobe.
+
+TenTen empowers you to display your best persona - hassle free.
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var Promise = require("bluebird");
+var async = require("async");
 var app = express();
 var fs = require("fs");
+var https = require('https');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,19 +13,157 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+   res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Cache-Control");
+   if (req.method === 'OPTIONS') {
+    res.statusCode = 204;
+    return res.end();
+  } else {
+    return next();
+  }
+});
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/getmestubdata', function (req, res) {
+app.get('/getStubData', function (req, res) {
    fs.readFile( __dirname + "/public/" + "data.json", 'utf8', function (err, data) {
-       console.log( data );
        res.end( data );
    });
 })
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/getTrends', function (req, response) {
+   console.log("getting customized trends...");
+   var http = require('http');
+   var url = "http://api.wunderground.com/api/b44b1327c40539f8/forecast10day/q";
+   var data = "";
+   
+   var options = {
+    protocol: 'http:'
+    , host: 'api.wunderground.com'
+    , port: 80
+    , path: '/api/b44b1327c40539f8/forecast10day/q/CA/San_Francisco.json'
+   };
+
+   var request = http.get(options, function(res) {
+        res.on('data', function(chunk){
+            data += chunk;
+        });
+        res.on('end', function() {
+            var dataobject = JSON.parse(data);
+            var weatherforecast = [];
+            for (var i = 0; i < 10; i++) { 
+                var temp = "day" + ':' + dataobject.forecast.simpleforecast.forecastday[i].date.day;
+                weatherforecast.push(temp);
+            }
+            console.log(weatherforecast);
+            response.end(JSON.stringify(weatherforecast));
+        });
+        res.on('errors', function(e){
+            console.log("There was an error: " + e.message)
+        });
+    });
+})
+
+
+app.get('/getOutlookEvents', function (req, response) {
+//   console.log("getting outlook events...");
+//   var http = require('http');
+//   var url = "https://outlook.office365.com/api/v1.0/me/events?$select=Subject,Organizer,Start,End";
+   var data = "";
+   
+   var options = {
+       host: 'outlook.office365.com',
+       path: '/api/v1.0/me/events?$select=Subject,Organizer,Start,End',
+        headers: {
+            'Authorization': 'Basic YWRtaW5AaHJvb2tkZW1vMTEub25taWNyb3NvZnQuY29tOnBhc3NAd29yZDE='
+        }
+   };
+   
+    // do the GET request
+    var reqGet = https.request(options, function(res) {
+        console.log("statusCode: ", res.statusCode);
+     
+        res.on('data', function(chunk) {
+            data += chunk;
+        });
+     
+    });
+     
+    reqGet.on('end', function(){
+        response.end(JSON.stringify(data));
+    });
+    reqGet.on('error', function(e) {
+        console.error(e);
+    });
+});
+
+app.get('/getWalmartData', function (req, response) {
+    var walmart = require('walmart')('s4uxqy9mcskf98g3gpzuvj2h');
+    var data = "";
+    // walmart.search("male summer").then(
+    //     function() {
+    //         response.end(data);
+    //     },
+    //     function(error) {
+    //         console.log("There was an error: " + error.message);
+    //     },
+    //     function(progress) {
+    //         data += progress;
+    //     }
+    // );
+    
+    walmart.search("summer").then(function(val) {
+        console.log(val.success);
+        response.end(val);
+    })
+    .catch(SyntaxError, function(e) {
+        console.error("unable to make the walmart api quert");
+    })
+    .catch(function(e) {
+        console.error("unable to make the search");
+    });
 });
 
 
+app.get('/get123',function(req,res) {
+    // Array to hold async tasks
+    var asyncTasks = [];
+    var data = "";
+
+    asyncTasks.push(function(callback){
+        for(var i = 1; i <10000; i++) {
+            data += i;
+            
+        }
+        callback();
+    });
+    
+    asyncTasks.push(function(callback){
+        for(var i = 10000; i <20000; i++) {
+            data += "xxxxxxx";
+            
+        }
+        callback();
+    });
+    
+    
+    // Now we have an array of functions doing async tasks
+    // Execute all async tasks in the asyncTasks array
+    async.parallel(asyncTasks, function(){
+        // All tasks are done now
+        data += "Infinity comes last"
+        res.end(data);
+    });
+});
+
+
+
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
